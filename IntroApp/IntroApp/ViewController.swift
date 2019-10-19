1517675f752c4cee5a115b47c93f3f053ab6580c
tsuyoposso <51411162+tsuyoposso@users.noreply.github.com>
2019-10-19 16:13:32 +0900

    first commit

---
+++ IntroApp/IntroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntroApp
+//
+//  Created by 長坂豪士 on 2019/10/19.
+//  Copyright © 2019 NagaKe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
